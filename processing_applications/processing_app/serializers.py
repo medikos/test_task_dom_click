451b9eb8e61a6c252ef9bc258080f33616b7e636
medik <medikos007@yandex.ru>
2021-07-22 17:26:16 +0300

    Add serializers

--- processing_applications/processing_app/serializers.py
+++ processing_applications/processing_app/serializers.py
@@ -1,11 +1,41 @@
 from rest_framework import serializers
-from processing_app.models import Application
+from processing_app.models import Application, Client, Employee, Position,StatusApplication
+
+
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StatusApplication
+        fields = ('name',)
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ('id', 'first_name', 'last_name', 'email', 'telegram_id')
+
+
+class PositionSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Position
+        fields = ('name',)
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    position = PositionSerializer()
+    class Meta:
+        model = Employee
+        fields = ('__all__')
+
+
+
 
 
 class ApplicationtSerializer(serializers.ModelSerializer):
     date_created  =  serializers.DateField(format='%d-%m-%y')
+    client = ClientSerializer( read_only=True)
+    employee = EmployeeSerializer(label='Hello')
+    status  = StatusSerializer()
     class Meta:
         model = Application
-        fields = ('id', 'status', 'date_created', 'client', 'employee','description', 'type')
+        fields = ('id', 'status', 'date_created','employee','client','description', 'type')
         read_only_fields = ('date_created','id', 'client')
         
